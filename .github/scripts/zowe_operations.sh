02bf7c9e809a15a6fbcbc95502d5dac88c5ba609
TJ-1060 <103194190+TJ-1060@users.noreply.github.com>
2025-03-07 12:11:16 +0100

    Update zowe_operations.sh

--- .github/scripts/zowe_operations.sh
+++ .github/scripts/zowe_operations.sh
@@ -11,14 +11,14 @@ pwd
 #if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" 
 #   &>/dev/null ; then
 
-if ! zowe zos-files list uss-files "//z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
-  echo "Directory does not exist. Creating it ..."
-  zowe zos-files create uss-directory "//z/$LOWERCASE_USERNAME/cobolcheck"
-else
-  echo "Directory already exists."
-fi
+#if ! zowe zos-files list uss-files "//z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+#  echo "Directory does not exist. Creating it ..."
+#  zowe zos-files create uss-directory "//z/$LOWERCASE_USERNAME/cobolcheck"
+#else
+#  echo "Directory already exists."
+#fi
 # Upload files
-zowe zos-files upload dir-to-uss "./cobol-check" "//z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar"
+#zowe zos-files upload dir-to-uss "./cobol-check" "//z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.9.jar"
 # Verify upload
-echo "Verifying upload: "
-zowe zos-files list uss-files "//z/$LOWERCASE_USERNAME/cobolcheck"
+#echo "Verifying upload: "
+#zowe zos-files list uss-files "//z/$LOWERCASE_USERNAME/cobolcheck"
