1f2d3eb45456292b2fb9c9b860cc4eed1997daff
Thomas Janisch <thomas.janisch@gmail.com>
2025-03-07 13:34:53 +0100

    Initial commit

---
+++ .github/scripts/zowe-operations.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# zowe_operations.sh
+# convert username to lowercase
+LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
+
+# check if dir exists
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+	echo "Directory does not exist. Creating it."
+	zowe zos-files create uss-directory /z/$LOWERCASE_USERNAME/cobolcheck
+else
+	echo "Directory already exists."
+fi
+
+#Upload Files
+
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.16.jar"
+
+#Verify upload
+echo "Verifying upload:"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+
