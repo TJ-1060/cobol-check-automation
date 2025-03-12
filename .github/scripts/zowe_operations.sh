2c6ee8046cb583649d01d50d47c1fa72465f683c
TJ-1060 <103194190+TJ-1060@users.noreply.github.com>
2025-03-12 11:21:30 +0100

    Test04

---
+++ .github/scripts/zowe_operations.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# zowe_operations.sh
+# convert username to lowercase
+# export PATH=$PATH:/usr/lpp/java/J8.0_64/bin
+# export PATH=$PATH:/usr/lpp/zowe/cli/node/bin
+echo $PATH
+export MSYS_NO_PATHCONV=1
+LOWERCASE_USERNAME=$(echo "$ZOWE_USERNAME" | tr '[:upper:]' '[:lower:]')
+
+# check if dir exists
+if ! zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck" &>/dev/null; then
+	echo "Directory does not exist. Creating it."
+	zowe zos-files create uss-directory "//z/$LOWERCASE_USERNAME/cobolcheck"
+else
+	echo "Directory already exists."
+fi
+
+#Upload Files
+
+zowe zos-files upload dir-to-uss "./cobol-check" "/z/$LOWERCASE_USERNAME/cobolcheck" --recursive --binary-files "cobol-check-0.2.16.jar"
+
+#Verify upload
+echo "Verifying upload:"
+zowe zos-files list uss-files "/z/$LOWERCASE_USERNAME/cobolcheck"
+
